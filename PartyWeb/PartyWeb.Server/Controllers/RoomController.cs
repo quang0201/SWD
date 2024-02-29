309502e3bab952abf1b72a2266832c979fbd446b
quang0201 <97324003+quang0201@users.noreply.github.com>
2024-02-29 09:55:59 +0700

    add IController

---
+++ PartyWeb/PartyWeb.Server/Controllers/RoomController.cs
@@ -0,0 +1,42 @@
+﻿using BusinessObjects.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Interface;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase, IFood
+    {
+        public List<Task<Food>> AddMany(List<Food> foods)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Food> AddOne(Food food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Food food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Food> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Task<Food>> Pagging(int page, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Food food)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PartyWeb/PartyWeb.Server/Interface/IUser.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObjects.Models;
+
+namespace Server.Interface
+{
+    public interface IUser
+    {
+        public Task<Account> GetAll();
+        public Task<Account> AddOne(Account decor);
+        public List<Task<Account>> AddMany(List<Account> decors);
+        public Task<bool> Update(Account decor);
+        public Task<bool> Delete(Account decor);
+        public List<Task<Account>> Pagging(int page, int pageSize);
+    }
+}
