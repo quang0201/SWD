49238f3051fdd404ce0e086b522f9744e9615769
Doanh9423 <nhatdoanhnguyen@gmail.com>
2024-03-18 23:25:08 +0700

    adding party host api

--- PartyWeb/PartyWeb.Server/Controllers/PartyHostController.cs
+++ PartyWeb/PartyWeb.Server/Controllers/PartyHostController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ModelViews.ModelView;
 using Services.Interface;
 
 namespace Server.Controllers
@@ -11,6 +12,86 @@ namespace Server.Controllers
         public PartyHostController(IPartyHostService service)
         {
             _service = service;
+        }
+
+        [HttpGet]
+        [Route("view-party-host/{id}")]
+        public async Task<IActionResult> ViewPartyHost(int id)
+        {
+            try
+            {
+                PartyHostModel? partyHost = await _service.GetPartyHostById(id);
+
+                if (partyHost == null)
+                {
+                    return NotFound();
+                }
+                return Ok(partyHost);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("add-party-host")]
+        public async Task<IActionResult> AddPartyHost([FromBody] PartyHostModel partyHost)
+        {
+            try
+            {
+                bool status = await _service.AddNewPartyHost(partyHost);
+
+                if (status)
+                {
+                    return Ok("Add party host succesful");
+                }
+                return BadRequest("Add party host failed");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("update-party-host")]
+        public async Task<IActionResult> UpdatePartyHost([FromBody] PartyHostModel partyHost)
+        {
+            try
+            {
+                bool status = await _service.UpdatePartyHost(partyHost);
+
+                if (status)
+                {
+                    return Ok("Update party host succesful");
+                }
+                return BadRequest("Update party host failed");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete-party-host/{id}")]
+        public async Task<IActionResult> DeletePartyHost(int id)
+        {
+            try
+            {
+                bool status = await _service.DeletePartyHost(id);
+
+                if (status)
+                {
+                    return Ok("Delete party host succesful");
+                }
+                return BadRequest("Delete party host failed");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
