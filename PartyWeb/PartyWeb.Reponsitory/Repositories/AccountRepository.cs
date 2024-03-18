7224092ca583cda4397efa199bce52bff259bf54
Doanh9423 <nhatdoanhnguyen@gmail.com>
2024-03-18 23:00:50 +0700

    adding remove account repo function

--- PartyWeb/PartyWeb.Reponsitory/Repositories/AccountRepository.cs
+++ PartyWeb/PartyWeb.Reponsitory/Repositories/AccountRepository.cs
@@ -8,10 +8,7 @@ namespace Reponsitories.Repositories
     {
         public Task<bool> AddNewAccount(Account account) => AccountDAO.Instance.AddAccount(account);
 
-        public Task<bool> DeleteAccount(string email)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<bool> DeleteAccount(string email) => AccountDAO.Instance.DeleteAccount(email);
 
         public Task<Account> GetAccountByEmail(string email)
         {
